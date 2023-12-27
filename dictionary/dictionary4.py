fa53cda06e29db166cc82301a4e21c7de7f65ef2
Mike Lau <michael_snowboarder@yahoo.com>
2023-12-26 21:17:41 -0800

    Dictionary exercise

---
+++ dictionary/dictionary4.py
@@ -0,0 +1,19 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+
+
+for x in thisdict.keys():
+  print(thisdict[x])
+
+for x in thisdict:
+  print(thisdict[x])
+
+
+
+print(thisdict.keys())
+print(thisdict.values())
