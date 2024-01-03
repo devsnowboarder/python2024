a15997ce40b6187446d186d866aa4c124dc7a687
Mike Lau <michael_snowboarder@yahoo.com>
2024-01-03 12:41:58 -0800

    Create customers.py

---
+++ APIExample/customers.py
@@ -0,0 +1,28 @@
+import requests
+import pprint
+
+URL ="https://pstmn-api-101.herokuapp.com/customers"
+
+URLGET ="https://pstmn-api-101.herokuapp.com/customer?id=1"
+
+reponse = requests.get(URL)
+print(reponse.status_code)
+print(reponse.raise_for_status())
+data = reponse.json()
+#print(data)
+print(data['welcome'])
+print(data['data'])
+print(data['data']['customers'])
+
+for x in range(3):
+   print(data['data']['customers'][x]['name'])
+   print(data['data']['customers'][x]['type'])
+
+reponse = requests.get(URLGET)
+print(reponse.status_code)
+print(reponse.raise_for_status())
+data = reponse.json()
+print(data)
+print(data['data'])
+print(data['data']['customer'])
+
